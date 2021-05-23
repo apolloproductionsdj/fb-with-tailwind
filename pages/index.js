2284ba5872e7e4032a0bd3d183bc192e5356d357
mike <apolloproductionsdj@gmail.com>
2021-05-23 13:50:24 -0700

    connected with Facebook to get user login availability and hide password and client_id to env file

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
-import Head from 'next/head'
-import Header from '../components/Header'
+import { getSession } from 'next-auth/client';
+import Head from 'next/head';
+import Header from '../components/Header';
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />
   return (
     <div>
       <Head>
         <title>Facebook</title>
 
       </Head>
+
       <Header />
       <main>
         {/* Sidebar */}
@@ -16,4 +19,14 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session
+    }
+  }
 }
